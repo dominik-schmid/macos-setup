a92f7ff82acd4eb2a3b3c16b74c461e2fa8e07ee
dominik-schmid <accounts@dominik-schmid.com>
2023-07-02 18:38:54 +0200

    Update scripts and description

---
+++ 2_0-backup-dotfiles.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+echo "Backup the dotfiles from the home directory using rsync to keep the directory structure and copy hidden files and folders"
+rsync -av --progress ~/ dotfiles/ ~
+
+echo "Reload .zshrc"
+source ~/.zshrc
