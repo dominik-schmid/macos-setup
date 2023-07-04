74f862ce9f6a1fe68195baa1f8105715381771c9
dominik-schmid <contact@dominik-schmid.com>
2023-07-04 19:59:47 +0200

    Made Dock appear instantly

--- 1-initial-setup.sh
+++ 1-initial-setup.sh
@@ -9,3 +9,7 @@ echo "Install Homebrew"
 echo "Install Ansible and Ansible lint"
 brew install ansible
 brew install ansible-lint
+
+# https://swissmacuser.ch/show-macos-dock-instantly-without-delay/
+echo "Configure Dock to show it instantly without a delay"
+defaults write com.apple.dock autohide-delay -float 0 && defaults write com.apple.dock autohide-time-modifier -float 0.4 && killall Dock
