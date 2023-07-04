b71d07fb44105956c20b2aa123bada501d0eb575
dominik-schmid <contact@dominik-schmid.com>
2023-07-04 20:03:45 +0200

    Moved Ansible installation to separate file
    because brew isn’t in the PATH variable at this moment

--- 1-initial-setup.sh
+++ 1-initial-setup.sh
@@ -5,11 +5,3 @@ xcode-select --install
 
 echo "Install Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-echo "Install Ansible and Ansible lint"
-brew install ansible
-brew install ansible-lint
-
-# https://swissmacuser.ch/show-macos-dock-instantly-without-delay/
-echo "Configure Dock to show it instantly without a delay"
-defaults write com.apple.dock autohide-delay -float 0 && defaults write com.apple.dock autohide-time-modifier -float 0.4 && killall Dock
---
+++ install-ansible.sh
@@ -0,0 +1,3 @@
+echo "Install Ansible and Ansible lint"
+brew install ansible
+brew install ansible-lint
