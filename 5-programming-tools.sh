b1d0735ab752143e0f16c2fb8eebd66157a05594
dominik-schmid <contact@dominik-schmid.com>
2023-07-04 20:05:50 +0200

    Moved VS Code and iTerm setup to separate file

--- 5-programming-tools.sh
+++ 5-programming-tools.sh
@@ -5,12 +5,3 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 echo "Install Node LTS version"
 n lts
-
-echo ""
-echo "Install VS Code config"
-chmod +x vscode-settings/install-config.sh
-./vscode-settings/install-config.sh
-
-echo ""
-echo "You need to configure iTerm2 to use the copied config. Go to iTerm2 -> Preferences -> General -> Preferences, enable 'Load preferences from a custom folder or URL'"
-echo "and use this folder: ~/.config/iterm2"
