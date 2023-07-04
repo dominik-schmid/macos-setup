41a8e37f08eeac9997b989a1700aa07808fd2f5c
dominik-schmid <contact@dominik-schmid.com>
2023-07-04 20:09:25 +0200

    Removed Proton Mail Bridge from downloads
    as it’ll be installed via Homebrew

--- 8-download-pkgs-and-dmgs.sh
+++ 8-download-pkgs-and-dmgs.sh
@@ -1,7 +1,4 @@
 #!/bin/zsh
 
-echo "Download Proton Mail Bridge"
-curl -o ~/Downloads/Proton-Mail-Bridge.dmg https://proton.me/download/bridge/Bridge-Installer.dmg
-
 echo "Download Synaptics DisplayLink Manager"
 curl -o ~/Downloads/DisplayLink-Manager.pkg https://www.synaptics.com/sites/default/files/exe_files/2023-03/DisplayLink%20Manager%20Graphics%20Connectivity1.8.1-EXE.pkg
