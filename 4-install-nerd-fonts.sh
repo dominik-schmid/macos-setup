3bf16c2b3ebda7ffa5d5d95120dfaf8581f2b2c3
dominik-schmid <contact@dominik-schmid.com>
2023-07-04 20:04:04 +0200

    Remove old font installation method

--- 4-install-nerd-fonts.sh
+++ 4-install-nerd-fonts.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
 brew tap homebrew/cask-fonts
+
 # When using `brew search nerd-font` there is at least one package that isn't a font. Therefore, I'm combining the search with grep.
 brew search font | grep "nerd" | xargs -I{} brew install --cask {} || true
-# brew search '/font-.\*-nerd-font/' | awk '{ print $1 }' | xargs -I{} brew install --cask {} || true
