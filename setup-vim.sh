ae7b5149923decfa8bbc553e31cbb3bb51a17132
DOSCHM2 <dominik.schmid@mercedes-benz.com>
2023-07-24 14:18:25 +0200

    Added vim plugin manager setup

---
+++ setup-vim.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+echo "Install Vim Plug Plugin Manager"
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
