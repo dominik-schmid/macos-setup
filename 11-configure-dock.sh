7a99daffd2d0277388e76f744cc3496b8ed112d7
DOSCHM2 <dominik.schmid@mercedes-benz.com>
2023-07-24 14:20:23 +0200

    Updated Dock setup

--- 11-configure-dock.sh
+++ 11-configure-dock.sh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+
 # https://swissmacuser.ch/show-macos-dock-instantly-without-delay/
 echo "Configure Dock to show it instantly without a delay"
-defaults write com.apple.dock autohide-delay -float 0 && defaults write com.apple.dock autohide-time-modifier -float 0 && killall Dock
+defaults write com.apple.dock autohide-time-modifier -int 0;killall Dock
